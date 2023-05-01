2acc01e20206fc664419333d6a0063101e181dd8
Leopold <570911+ostrich@users.noreply.github.com>
2023-05-01 19:24:40 -0400

    modified:   gzdoom-nightly.py

--- gzdoom-nightly.py
+++ gzdoom-nightly.py
@@ -26,20 +26,20 @@ gzdoom_links = [link for link in links if link.has_attr('href') and link['href']
 sorted_links = sorted(gzdoom_links, key=lambda x: tuple(int(part) if part.isdigit() else part for part in x['href'].split('-')[2:]))
 
 # Get the link with the highest version number and build number
-latest_link = sorted_links[-1]['href'].split('/gzdoom/')[-1]
+latest_file = sorted_links[-1]['href'].split('/gzdoom/')[-1]
 
 # Print the version and build number of the file to be downloaded
-version, build = latest_link.split('-')[2:4]
+version, build = latest_file.split('-')[2:4]
 print(f"Downloading GZDoom version {version} build {build}...")
 
 # Download the latest gzdoom archive file
-response = requests.get(url + latest_link)
+response = requests.get(url + latest_file)
 
 if response.status_code != 200:
-    print(f"Error: Failed to download {url+latest_link}. Status code: {response.status_code}")
+    print(f"Error: Failed to download {url+latest_file}. Status code: {response.status_code}")
     exit()
 
-filename = latest_link.split('/')[-1]
+filename = latest_file.split('/')[-1]
 with open(filename, 'wb') as f:
     f.write(response.content)
 
